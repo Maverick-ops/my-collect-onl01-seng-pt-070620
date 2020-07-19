14bf1ba6b593225813a14aebd6c5f1c0aeec2c3e
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-19 23:39:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,12 @@
 def my_collect(languages) do |language|
  language.upcase
- 
+ empty_array = []
+    counter = 0
+      my_collect(empty_array) do |x|
+        counter += 1
+      end
+    expect(counter).to eq(0)
+  end
 end
 
 
