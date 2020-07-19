d8c424f954d86f681bb3b72d438f7260dd789428
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-19 23:46:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,6 @@
 languages = ['ruby', 'javascript', 'python', 'objective-c']
-def my_collect(languages)  do |lang|
-     lang.upcase
-       end
- end
+def my_collect(languages) 
+  
 
 def my_collect(students) do |student|
          student.split(" ").first
