62aca3eac8221599f76b3ee0543e73a4f3c3678d
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-19 23:46:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,13 @@
 languages = ['ruby', 'javascript', 'python', 'objective-c']
 def my_collect(languages) 
-  
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
 def my_collect(students) do |student|
          student.split(" ").first
